3aa12884a9df77d50f5d7e0226b4d54fb2f25b2a
Ömer Aydemir <omercanaydemir1@gmail.com>
2025-02-27 21:05:10 +0300

    Created core app where backend models,views and configs will be implemented

--- favor_backend/settings.py
+++ favor_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qzp+r#hc&kxo05034%7f0zd%kq(*)tccsz2y%y^kkl*r7ct^r$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core'
 ]
 
 MIDDLEWARE = [
