2f23da5b618b4cb07e4b1bb1acaa64512ac39265
Ömer Aydemir <omercanaydemir1@gmail.com>
2025-03-13 10:03:05 +0300

    Basic JWT Authentication logic is configured

--- favor_backend/settings.py
+++ favor_backend/settings.py
@@ -40,8 +40,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    'rest_framework',
+    'rest_framework_simplejwt',
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
