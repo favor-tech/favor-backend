881acaaacdf9628b993badf3b8231b8ed3912021
Ömer Aydemir <omercanaydemir1@gmail.com>
2025-05-18 00:01:29 +0300

    Audit mododel improvements

--- core/models/audit.py
+++ core/models/audit.py
@@ -1,8 +1,9 @@
 from .base import BaseModel
 from django.db import models
+from django.conf import settings
 
 class Audit(BaseModel):
-    user = models.ForeignKey("User", on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     action = models.CharField(max_length=255,db_index=True)
     table_name = models.CharField(max_length=255)
     ip_address = models.GenericIPAddressField(null=True, blank=True)
