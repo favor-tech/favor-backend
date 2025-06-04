fda4e7ce41bb0e73b77b2cedb67841ede50e8aa2
Ömer Aydemir <omercanaydemir1@gmail.com>
2025-06-04 21:56:42 +0300

    Blacklist improvements

--- core/models/blacklist.py
+++ core/models/blacklist.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 class Blacklist(BaseModel):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    banned_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="banned_users")
+    banned_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True,blank=True, related_name="banned_users")
     reason = models.TextField()
     ban_type = models.CharField(max_length=10, choices=[("TEMP", "Temporary"), ("PER", "Permanent")])
     banned_until = DateTimeField(null=True, blank=True)
