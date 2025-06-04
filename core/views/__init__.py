e43208558533335a348c41637362ca7bf766ce64
Ömer Aydemir <omercanaydemir1@gmail.com>
2025-06-04 22:02:17 +0300

    Missing views are added to init

--- core/views/__init__.py
+++ core/views/__init__.py
@@ -3,3 +3,8 @@ from .gallery import GalleryView
 from .events import EventsView
 from .event_detail import EventDetailView
 from .bookmark import BookmarkView
+from .artist_profile import ArtistProfileView
+from .user_profile import UserProfileView
+from .bookmark_list import BookmarkListView
+from .gallery_profile import GalleryProfileView
+from .account_settings import AccountSettingsView
