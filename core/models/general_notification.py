366d102b0597fb41febf60de6f8bdc8060cfaad1
Ömer Aydemir <omercanaydemir1@gmail.com>
2025-07-29 22:31:16 +0300

    Notification and General Notification models are improved,new fields are added

--- core/models/general_notification.py
+++ core/models/general_notification.py
@@ -4,6 +4,12 @@ from django.db import models
 class GeneralNotification(BaseModel):
     title = models.CharField(max_length=255)
     message = models.TextField()
+    redirect_url = models.URLField(null=True, blank=True)
+    is_sent = models.BooleanField(default=False)
 
     class Meta:
         db_table = "general_notification"
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.title
--- core/models/notification.py
+++ core/models/notification.py
@@ -2,6 +2,7 @@ from .base import BaseModel
 from django.db import models
 from .definitions.DateTimeWithoutTZ import DateTimeWithoutTZField as DateTimeField
 from django.conf import settings
+from django.utils import timezone
 
 class Notification(BaseModel):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
@@ -9,6 +10,8 @@ class Notification(BaseModel):
     message = models.TextField()
     is_read = models.BooleanField()
     read_at = DateTimeField(null=True, blank=True)
-
+    redirect_url = models.URLField(max_length=500, null=True, blank=True)
+    sent_at = DateTimeField(default=timezone.now)
     class Meta:
         db_table = "notification"
+        ordering = ["-sent_at"]
